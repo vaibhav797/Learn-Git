7dc32ca803376496919423f5a56f88587cb3da59
Vaibhav Kapoor <vaibhavkapoor.480@gmail.com>
2022-01-12 23:07:38 +0530

    added the separate function for sum

--- sum.cpp
+++ sum.cpp
@@ -1,13 +1,17 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+void add(int a, int b)
+{
+    cout<<(a+b)<<endl;
+}
+
 int main()
 {
     int a=5, b=10;
 
     int sum = a+b;
 
-    cout<<sum<<endl;
-    
+    cout<<sum<<endl;    
     return 0;
 }
